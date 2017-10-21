f2d0bd1d6bc6815387d2e2730b903d625980f449
Zhao <shixiang.zhao@cnt01.local>
2017-10-21 17:17:00 +0800

    hello world for scala

---
+++ src/com/shixzh/spark/examples/HelloWorld.scala
@@ -0,0 +1,5 @@
+package com.shixzh.spark.examples
+
+object HelloWorld extends App {
+  println("Hello, World!")
+}
---
+++ src/com/shixzh/spark/examples/SparkPi.scala
@@ -0,0 +1,5 @@
+package com.shixzh.spark.examples
+
+object SparkPi extends App {
+  println("Hello, World!")
+}
